c9b92521335fc41bbc4ee39704af84b8b3237071
Fangtastic7 <league@iMac-2.attlocal.net>
2018-10-12 18:30:16 -0700

    Binary Converter

--- src/BinaryConverter.java
+++ src/BinaryConverter.java
@@ -14,75 +14,79 @@ public class BinaryConverter implements ActionListener, MouseListener {
 	public static void main(String[] args) {
 		new BinaryConverter().createUI();
 	}
-	 JFrame frame = new JFrame();
-	 JPanel panel = new JPanel();
-	 JTextField input1 = new JTextField(20);
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JTextField input1 = new JTextField(20);
 	JLabel output = new JLabel();
 	JButton button1 = new JButton();
-	
+
 	private void createUI() {
-	
-	frame.add(panel);
-	frame.setTitle("Binary Converter");
-	frame.setVisible(true);
-	input1.getText();
-	button1.setText("Convert");
-	panel.add(input1);
-	
-	panel.add(button1);
-	panel.add(output);
-	button1.addMouseListener(this);
-	frame.pack();
-}
 
-	
-	 String convert(String input) {
-		 
-         if(input.length() != 8){
-              JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
-              return "-";
-         }
-         String binary = "[0-1]+";    //must contain numbers in the given range
-         if (!input.matches(binary)) {
-              JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
-              return "-";
-         }
-          try {
-        	 String value = "1";
-        	 int total = 1;
-        	 int sum =0;
-              input.length();
-              
-              for(int i=0;i<8; i++) {
-            	 
-            	 
-            		 if(i==0) {
-            			 total =1;
-            		 }
-            		 else {
-            			 total = total *2;            	  
-            		 }
-            		 if(value.equals(input.charAt(i))) {
-            	  sum = sum +total;
-            	  System.out.println(sum);
-              }
-              }
-         } catch (Exception e) {
-              JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
-              return "-";
-         }
-}
+		frame.add(panel);
+		frame.setTitle("Binary Converter");
+		frame.setVisible(true);
+		input1.getText();
+		button1.setText("Convert");
+		panel.add(input1);
 
+		panel.add(button1);
+		panel.add(output);
+		button1.addMouseListener(this);
+		frame.pack();
+	}
 
-	
+	String convert(String input) {
 
+		if (input.length() != 8) {
+			JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+			return "-";
+		}
+		String binary = "[0-1]+"; // must contain numbers in the given range
+		if (!input.matches(binary)) {
+			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+			return "-";
+		}
+		try {
+			char value = '1';
+			int total = 1;
+			int sum = 0;
+
+			for (int i = 0; i < 8; i++) {
+
+				if (value == (input.charAt(i))) {
+
+					sum = sum + exponent(i);
+					System.out.println(sum);
+				}
+			}
+			System.out.println(sum);
+			return "-";
+
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+			return "-";
+		}
+	}
+
+	private int exponent(int num) {
+		// TODO Auto-generated method stub
+		int outcome = 1;
+		
+
+			for (int i = 0; i < num; i++) {
+				outcome = outcome * 2;
+			
+		}
+		
+		return outcome;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
@@ -92,34 +96,28 @@ public class BinaryConverter implements ActionListener, MouseListener {
 		frame.pack();
 	}
 
-
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
-	
 }
